8b737be6dc04685f53b912b24bd97d91c16c50fb
Phillip Creglow <phillipcreglow@msn.com>
2015-06-09 21:11:15 -0400

    Error messages contine with jQuery...BLOCKER

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,1 @@
-$.getJSON('../api/github/users/octocat/octocat.json').then(function(octocat) {
 
-    console.log(octocat.avatar_url);
-
-    $avatar = $('img#avatar');
-
-    $avatar.attr('src', octocat.avatar_url);
-
-    $('[data-bind="user.login"]').text(octocat.login)
-
-  });
