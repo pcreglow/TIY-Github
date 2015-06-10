d48e35d4b7788fca62daf4c466288332d437fb56
Phillip Creglow <phillipcreglow@msn.com>
2015-06-10 16:42:12 -0400

    "Finally replaced the placeholders! "

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
+$.getJSON('../api/github/users/octocat.json')
+.then(function(octocat) {
 
+    console.log(octocat.avatar_url);
+
+    $avatar = $('img#avatar');
+
+    $avatar.attr('src', octocat.avatar_url);
+
+    $('[data-bind="user.name"]').text(octocat.name);
+
+    $('[data-bind="user.login"]').text(octocat.login);
+
+    $('[data-bind="user.following"]').text(octocat.following);
+
+    $('[data-bind="user.followers"]').text(octocat.followers);
+
+    $('[data-bind="user.company"]').text(octocat.company);
+
+    $('[data-bind="user.location"]').text(octocat.location);
+
+    $('[data-bind="user.email"]').text(octocat.email);
+
+    $('[data-bind="user.blog"]').text(octocat.blog);
+
+  });
